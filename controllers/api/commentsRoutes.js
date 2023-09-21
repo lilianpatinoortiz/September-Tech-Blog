bf8adff445e77acfcd6c4133a4497b8cfb6d7137
Lilian Patino Ortiz <lilian@Lilians-MacBook-Pro.local>
2023-09-20 17:40:22 -0700

    Comments working now

--- controllers/api/commentsRoutes.js
+++ controllers/api/commentsRoutes.js
@@ -50,6 +50,7 @@ router.post("/", async (req, res) => {
   try {
     const newComment = await Comment.create({
       ...req.body,
+      user_id: req.session.user_id,
     });
 
     res.status(200).json(newComment);
